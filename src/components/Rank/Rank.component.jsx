b4710f97c44c1c94e0c6ccd038462ecd9ca2aeeb
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-08-08 17:51:16 +0100

    Added a navigation and rank components.

---
+++ src/components/Rank/Rank.component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Rank.style.css';
+
+const Rank = () => {
+    return (
+        <div className='center'>
+            <p class='paragraph'>Welcome! Your rank is ...</p>
+        </div>
+    )
+}
+
+export default Rank;
--- src/containers/App.js
+++ src/containers/App.js
@@ -1,7 +1,12 @@
+import Navigation from '../components/Navigation/Navigation.component';
+import Rank from '../components/Rank/Rank.component';
+
 function App() {
   return (
     <div>
-
+      <Navigation />
+      <Rank />
+      {/*<ImageLink />*/}
     </div>
   );
 }
