5c2a4eef2bff3e05be6fed32cf3c0cb167232f7e
Andy Bowskill <AndyBowskill@users.noreply.github.com>
2021-08-13 17:19:54 +0100

    Added the face detection box.

--- src/components/FaceRecognition/FaceRecognition.component.jsx
+++ src/components/FaceRecognition/FaceRecognition.component.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import './FaceRecognition.style.css';
 
-const FaceRecognition = ({imageURL}) => {
-    return (
-        <div className="face-box flex">
-            <img alt='' src={imageURL}  />
-        </div>
-    )
-}
+const FaceRecognition = ({ imageURL, faceBox }) => {
+  return (
+      <div className='face-box flex'>
+        <img id='face-recognition-image' alt='Face recognition' src={imageURL} width='500px' height='auto' />
+        <div className='boundary-box' style={{top: faceBox.top_row, right: faceBox.right_column, bottom: faceBox.bottom_row, left: faceBox.left_column}}></div>
+      </div>
+  );
+};
 
 export default FaceRecognition;
--- src/containers/App.js
+++ src/containers/App.js
@@ -3,6 +3,11 @@ import Navigation from '../components/Navigation/Navigation.component';
 import Rank from '../components/Rank/Rank.component';
 import ImageLinkForm from '../components/ImageLinkForm/ImageLinkForm.component';
 import FaceRecognition from '../components/FaceRecognition/FaceRecognition.component';
+import Clarifai from 'clarifai';
+
+const clarifaiApp = new Clarifai.App({
+  apiKey: '32ae7e3365914151b4459c0565d870b4',
+});
 
 class App extends React.Component {
   constructor() {
@@ -10,8 +15,32 @@ class App extends React.Component {
     this.state = {
       input: '',
       imageURL: '',
+      faceBox: {},
     };
   }
+
+  calculateFaceLocation = (clarifaiData) => {
+    const clarifaiRegionInfo =
+      clarifaiData.outputs[0].data.regions[0].region_info;
+    const faceRecognitionImage = document.getElementById(
+      'face-recognition-image'
+    );
+    const width = Number(faceRecognitionImage.width);
+    const height = Number(faceRecognitionImage.height);
+
+    return {
+      left_column: clarifaiRegionInfo.bounding_box.left_col * width,
+      right_column:
+        width - (clarifaiRegionInfo.bounding_box.right_col * width),
+      top_row: clarifaiRegionInfo.bounding_box.top_row * height,
+      bottom_row: height - (clarifaiRegionInfo.bounding_box.bottom_row * height),
+    };
+  };
+
+  updateFaceBox = (box) => {
+    console.log(box)
+    this.setState({ faceBox: box });
+  };
 
   onInputChange = (event) => {
     this.setState({ input: event.target.value });
@@ -19,6 +48,15 @@ class App extends React.Component {
 
   onButtonClick = () => {
     this.setState({ imageURL: this.state.input });
+
+    clarifaiApp.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) => {
+        console.log(response);
+        this.updateFaceBox(this.calculateFaceLocation(response))
+      }
+      )
+      .catch((error) => console.log(error));
   };
 
   render() {
@@ -30,7 +68,7 @@ class App extends React.Component {
           onInputChange={this.onInputChange}
           onButtonClick={this.onButtonClick}
         />
-        <FaceRecognition imageURL={this.state.imageURL} />
+        <FaceRecognition imageURL={this.state.imageURL} faceBox={this.state.faceBox} />
       </div>
     );
   }
